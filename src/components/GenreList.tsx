ae9e4c7d65b999ca3818a697dd6f41b2b5d17517
Ganesh Palve <gpalve@gmail.com>
2023-05-04 13:16:30 +0530

    Added spinner to load genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,35 @@
-import useGenres, { Genre } from "../hooks/useGenres";
+import {
+  Center,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/imageUrl";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  if (isLoading) return <Spinner title="Loading Genres" color="green" />;
+  if (error) return null;
   return (
-    <ul>
-      {data.map((gen) => (
-        <li key={gen.id}>{gen.name}</li>
+    <List>
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY={1}>
+          <HStack>
+            <Image
+              src={getCroppedImageUrl(genre.image_background)}
+              boxSize="32px"
+              borderRadius={8}
+              padding={1}
+            ></Image>
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
